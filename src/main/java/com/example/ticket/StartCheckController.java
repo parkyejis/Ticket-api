a941b9c12b348d1e5cf965def8d1bf0a97d68dee
ontte0322@skuniv.ac.kr <ontte0322@skuniv.ac.kr>
2026-03-10 13:23:36 +0900

    feat: 시작 전 작동 확인을 위한 controller 생성

--- src/main/java/com/example/ticket/StartCheckController.java
+++ src/main/java/com/example/ticket/StartCheckController.java
@@ -1,4 +1,14 @@
 package com.example.ticket;
 
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StartCheckController {
+
+    @GetMapping("/api/Start")
+    public String StartCheck(){
+        return "🔥 Ticket API Server is perfectly running! 🔥";
+    }
 }
