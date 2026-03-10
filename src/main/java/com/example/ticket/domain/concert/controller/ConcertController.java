34c1864a6a05bdb0b0f6b4a53139a02befd6dc0d
ontte0322@skuniv.ac.kr <ontte0322@skuniv.ac.kr>
2026-03-10 13:26:06 +0900

    feat: 공연 목록 조회 Service 및 Controller 구현

---
+++ src/main/java/com/example/ticket/domain/concert/controller/ConcertController.java
@@ -0,0 +1,42 @@
+package com.example.ticket.domain.concert.controller;
+
+import com.example.ticket.domain.concert.dto.request.ConcertRequestDto;
+import com.example.ticket.domain.concert.repository.ConcertRepository;
+import com.example.ticket.domain.concert.service.ConcertService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/concert")
+public class ConcertController {
+
+    private final ConcertService concertService;
+
+    //생성
+    @PostMapping("/")
+    public void createConcert(@RequestBody ConcertRequestDto dto){
+        //concertService.createConcert(dto);
+    }
+
+    //읽기
+    @GetMapping("/list")
+    public void getConcertList(){
+        concertService.getConcertList();
+    }
+
+    @GetMapping("/detail")
+    public void getConcertDetail(){
+        concertService.getConcertDetail();
+    }
+
+    //수정하기
+    public void updateConcert(){
+
+    }
+
+    //지우기
+    public void deleteConcert(){
+
+    }
+}
---
+++ src/main/java/com/example/ticket/domain/concert/service/ConcertService.java
@@ -0,0 +1,24 @@
+package com.example.ticket.domain.concert.service;
+
+import com.example.ticket.domain.concert.dto.response.ConcertResponseDto;
+import com.example.ticket.domain.concert.repository.ConcertRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ConcertService {
+
+    private final ConcertRepository concertRepository;
+
+    //존재하는 콘서트 리스트 가져오기
+    public void getConcertList() {
+        //검사해줘야하는 것 end 날짜가 지났는지 확인
+        LocalDateTime now = LocalDateTime.now();
+        List<ConcertResponseDto> list = concertRepository.findByList(now);
+    }
+}
