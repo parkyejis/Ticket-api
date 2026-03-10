93bdec469a349b0106827e33d0780fa96ed0c926
ontte0322@skuniv.ac.kr <ontte0322@skuniv.ac.kr>
2026-03-10 13:24:15 +0900

    feat: Concert 엔티티 생성 및 endDate 인덱스 추가

---
+++ src/main/java/com/example/ticket/domain/concert/entity/Concert.java
@@ -0,0 +1,53 @@
+package com.example.ticket.domain.concert.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "concert", indexes = {
+        @Index(name = "idx_end_Date", columnList = "endDate") //endDate를 기준으로 정렬
+})
+public class Concert {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "concertId", nullable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "Detail", nullable = false)
+    private String Detail;
+
+    @Column(name = "startDate", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "endDate", nullable = false)
+    private LocalDateTime endDate;
+
+    @Column(name = "progressTime", nullable = false)
+    private Long progressTime;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    @Column(name = "state", nullable = false)
+    private concertState state = concertState.PROGRESS;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "imgURL")
+    private String imgURL;
+
+}
